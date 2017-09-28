b5446f62f089c0e3cb028c10ae6ed6bfa0c1e9fa
Hongshan Li <lihongshan1989@outlook.com>
2017-09-28 11:39:05 -0400

    latest parameters

--- hw5/img2obj.py
+++ hw5/img2obj.py
@@ -12,13 +12,14 @@ import numpy as np
 import pickle
 import cv2
 from PIL import Image
+import matplotlib.pyplot as plt
 
 parser = argparse.ArgumentParser(description='Lenet on CIFAR100')
 parser.add_argument("--batch_size", type=int, default=128, metavar="N",
     help="mini-batch size")
 parser.add_argument("--epoch", type=int, default=10, metavar="N",
     help="number of epochs to train")
-parser.add_argument("--lr", type=float, default=1e-5, metavar="LR",
+parser.add_argument("--lr", type=float, default=0.1, metavar="LR",
     help="learning rate")
 parser.add_argument("--momentum", type=float, default=0.5, metavar="M",
     help="SGD momentum")
@@ -43,9 +44,9 @@ class LeNet(nn.Module):
         self.fc2 = nn.Linear(512, 256)
         self.fc3 = nn.Linear(256, 100)
         self.optim = optim.SGD(self.parameters(), lr=args.lr, momentum=args.momentum)
-        self.training_loss = 0
-        self.test_loss = 0
-        self.accuracy = 0
+        self.training_loss = []
+        self.accuracy = []
+
 
     def forward(self, x):
         if isinstance(x, np.ndarray):
@@ -107,7 +108,7 @@ class LeNet(nn.Module):
         train_data_loader = DataLoader(train_data, batch_size = args.batch_size,
             shuffle=True)
 
-        
+        current_loss = 0
         for batch_idx, (data, target) in enumerate(train_data_loader):
             target = Variable(target)
             self.optim.zero_grad()
@@ -115,13 +116,15 @@ class LeNet(nn.Module):
             loss = F.nll_loss(pred, target)
             loss.backward()
             self.optim.step()
-            self.training_loss += loss
+            current_loss += loss
 
             if batch_idx % args.log_interval==0:
                 print("Step: %d, negative log loss %f" % (batch_idx, loss.data[0]))
         
         # compute the average training loss
-        self.training_loss = self.training_loss / len(train_data_loader)        
+        current_loss = current_loss.data.numpy()
+        current_loss = np.asscalar(current_loss)
+        self.training_loss.append(current_loss / len(train_data_loader))        
         
 
     
@@ -136,17 +139,50 @@ class LeNet(nn.Module):
         for data, target in test_data_loader:
             target = Variable(target)
             pred = self.forward(data)
-            self.test_loss = F.nll_loss(pred, target, size_average=False).data[0]
             pred = pred.data.max(1, keepdim=True)[1]
             correct = pred.eq(target.data.view_as(pred)).cpu().sum()
 
-        self.accuracy = float(correct) / float(len(test_data))
-        print("The accuracy is:%f" % self.accuracy)
+        self.accuracy.append(float(correct) / float(len(test_data)))
+        print("The accuracy is:%f" % self.accuracy[-1])
+
+    def summarize(self):
+        num_epoch = len(self.accuracy)
+        epoch = range(1, num_epoch+1)
+        # save training loss
+        training_loss = np.array(self.training_loss)
+        np.save("TrainingLoss.npy", training_loss)
+        test_accuracy = np.array(self.accuracy)
+        np.save("TestAccuracy.npy", test_accuracy)
+
+        plt.scatter(epoch, self.training_loss, s=10, c="b", 
+            marker="s", label="Average Training Error")
+        plt.legend(loc="upper left")
+        plt.savefig("AverageTrainingError.png")
+        plt.gcf().clear()
+
+        plt.scatter(epoch, self.accuracy, s=10, c="r",
+            marker="s", label="Accuracy on Test Set")
+        plt.legend(loc="upper left")
+        plt.savefig("AccuracyOnTestSet.png")
+        plt.gcf().clear()
+
+        
+
 
             
 
             
 
+if __name__ == "__main__":
+    a = LeNet()
+ 
+    for epoch in range(1, args.epoch+1):
+        if epoch % 10 ==1 and epoch > 10:
+            args.lr = args.lr / 10       
+        a.train()
+        torch.save(a.state_dict(), "latest_parameters.pt")
+        a.evaluate()
+    
+    a.summarize()
+    
 
-a = LeNet()
-a.cam()
